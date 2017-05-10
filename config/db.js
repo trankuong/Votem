6e47bf19f720e7e29ca82f3188309bca9ea85587
Kuong Tran <kuong@umich.edu>
2017-05-10 17:17:51 -0400

    debugging check connecting to database

--- config/db.js
+++ config/db.js
@@ -1,4 +1,8 @@
 module.exports = {
-	url : (process.env.MONGOLAB_URI || 'mongodb://localhost/Votem_db'),
-	login: {user: "server", pass: "1VCnSCvzsB5KACZdxHoKeIKHqwzRBa"}
+	// For local use
+	//url : (process.env.MONGOLAB_URI || 'mongodb://localhost/Votem_db'),
+
+	// For aws 
+	url   : '0.0.0.0:27017',
+	login : {user: "server", pass: "1VCnSCvzsB5KACZdxHoKeIKHqwzRBa"}
 }
