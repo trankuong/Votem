256f5de9f47cf68da313d074462c0c87cc7684ee
Kuong Tran <kuong@umich.edu>
2017-05-10 15:10:52 -0400

    connect to db

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-	url : (process.env.MONGOLAB_URI || 'mongodb://localhost/Votem_db')
+	url : (process.env.MONGOLAB_URI || 'mongodb://localhost/Votem_db'),
+	login: {user: "", pass: "1VCnSCvzsB5KACZdxHoKeIKHqwzRBa"}
 }
